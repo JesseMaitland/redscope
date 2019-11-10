da339c49dcf86a47d36c1999a7840b5574ed409c
jessemaitland <jesse.maitland@heyjobs.de>
2019-11-10 21:46:09 +0100

    work on deployment

--- setup.py
+++ setup.py
@@ -1,24 +1,35 @@
-from os import linesep
+import os
+import sys
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+VERSION = '0.1.0'
 
 
-def get_requirements():
-    with open('requirements.txt') as file:
-        return [line.rstrip(linesep) for line in file.readlines()]
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(tag, VERSION)
+            sys.exit(info)
 
 
-def get_long_description():
+def readme():
     with open('README.md') as file:
         return file.read()
 
 
 setup(
     name='redscope',
-    version='0.1.0',
+    version=VERSION,
     author='Jesse Maitland',
     discription='A database migration and introspection tool for AWS Redshift',
     include_package_data=True,
-    long_description=get_long_description(),
+    long_description=readme(),
     install_requires=[
         'rambo-py',
         'psycopg2-binary',
@@ -29,5 +40,9 @@ setup(
     packages=find_packages(exclude=('tests*', 'venv', 'database*')),
     scripts=["bin/redscope"],
     download_url="",
-    long_description_content_type="text/markdown"
+    long_description_content_type="text/markdown",
+    python_requires='>=3',
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
 )
