914fa2f56a98fb1209c057745ce14db1def8d402
jessemaitland <jesse.maitland@heyjobs.de>
2019-11-10 22:34:17 +0100

    blah

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-VERSION = '0.1.2'
+VERSION = '0.1.3'
 
 
 class VerifyVersionCommand(install):
