eb52051154b899ae7b1693fe694b610f1b0640e5
jessemaitland <jesse.maitland@heyjobs.de>
2019-11-27 18:08:26 +0100

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-VERSION = '0.1.7'
+VERSION = '0.1.8'
 
 
 class VerifyVersionCommand(install):
