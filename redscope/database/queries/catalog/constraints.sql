74a7962c8924c78ce75903efbdbf8c9655260f47
jessemaitland <jesse.maitland@heyjobs.de>
2019-11-27 09:43:09 +0100

    table introspect improved

--- redscope/database/queries/catalog/constraints.sql
+++ redscope/database/queries/catalog/constraints.sql
@@ -2,7 +2,8 @@
 SELECT ns.nspname as schema_name,
        c.relname  as table_name,
        a.attname  as column_name,
-       pg_get_constraintdef(cn.oid) as con_def
+       pg_get_constraintdef(cn.oid) as con_def,
+       cn.conname as con_name
   FROM pg_constraint cn
        INNER JOIN pg_namespace ns
           ON ns.oid = cn.connamespace
--- redscope/introspection/tables.py
+++ redscope/introspection/tables.py
@@ -15,7 +15,8 @@ class IntroTables:
 
     def execute(self):
         self.fetch_data()
-        self._merge_tables_and_constraints()
+        self._make_constraint_sql()
+
         self._format_sql()
         self._set_new_columns()
         self._set_default_and_null()
@@ -23,6 +24,8 @@ class IntroTables:
         self._make_column_sql()
         self._remove_whitespace()
         self._group_by_table()
+        self._merge_tables_and_constraints()
+        self._nan_to_mt_string()
         self._set_create_table()
         self._set_paths()
         self._make_dirs()
@@ -36,50 +39,57 @@ class IntroTables:
         self.table_const = self.tables.merge(self.const, how='left')
 
     def _format_sql(self):
-        self.table_const['data_type'] = self.table_const['data_type'].str.replace('character varying', 'VARCHAR')
-        self.table_const['data_type'] = self.table_const['data_type'].str.replace('character', 'CHAR')
-        self.table_const['data_type'] = self.table_const['data_type'].str.replace('integer', 'INT')
-        self.table_const['data_type'] = self.table_const['data_type'].str.replace('timestamp without time zone',
-                                                                                  'TIMESTAMP')
-        self.table_const['data_type'] = self.table_const['data_type'].str.replace('timestamp with time zone',
-                                                                                  'TIMESTAMPZ')
-        self.table_const['data_type'] = self.table_const['data_type'].str.upper()
+        self.tables['data_type'] = self.tables['data_type'].str.replace('character varying', 'VARCHAR')
+        self.tables['data_type'] = self.tables['data_type'].str.replace('character', 'CHAR')
+        self.tables['data_type'] = self.tables['data_type'].str.replace('integer', 'INT')
+        # self.table_const['data_type'] = self.table_const['data_type'].str.replace('timestamp without time zone',
+        #                                                                           'TIMESTAMP')
+        # self.table_const['data_type'] = self.table_const['data_type'].str.replace('timestamp with time zone',
+        #                                                                           'TIMESTAMPZ')
+        self.tables['data_type'] = self.tables['data_type'].str.upper()
 
     def _set_new_columns(self):
-        self.table_const['sql_null'] = pd.np.nan
-        self.table_const['dflt_sql'] = pd.np.nan
-        self.table_const['col_sql'] = pd.np.nan
+        self.tables['sql_null'] = pd.np.nan
+        self.tables['dflt_sql'] = pd.np.nan
+        self.tables['col_sql'] = pd.np.nan
 
     def _set_default_and_null(self):
-        self.table_const.loc[self.table_const['not_null'] == True, 'sql_null'] = "NOT NULL"
-        self.table_const.loc[self.table_const['has_default'] == True, 'dflt_sql'] = "DEFAULT"
+        self.tables.loc[self.tables['not_null'] == True, 'sql_null'] = "NOT NULL"
+        self.tables.loc[self.tables['has_default'] == True, 'dflt_sql'] = "DEFAULT"
+
+    def _make_constraint_sql(self):
+        self.const['const_sql'] = "CONSTRAINT " + self.const['con_name'] + " " + self.const['con_def']
 
     def _nan_to_mt_string(self):
+        self.tables = self.tables.fillna('')
         self.table_const = self.table_const.fillna('')
 
     def _remove_whitespace(self):
-        self.table_const['col_sql'] = self.table_const['col_sql'].str.strip()
+        self.tables['col_sql'] = self.tables['col_sql'].str.strip()
+        self.const['const_sql'] = self.const['const_sql'].str.strip()
 
     def _make_column_sql(self):
-        self.table_const['col_sql'] = self.table_const['column_name'] + ' ' + self.table_const['data_type'] + ' ' + \
-                                      self.table_const['sql_null'] + ' ' + \
-                                      self.table_const['dflt_sql'] + ' ' + self.table_const['default_value'] + ' ' + \
-                                      self.table_const['con_def']
+        self.tables['col_sql'] = self.tables['column_name'] + ' ' + self.tables['data_type'] + ' ' + \
+                                 self.tables['sql_null'] + ' ' + \
+                                 self.tables['dflt_sql'] + ' ' + self.tables['default_value']
 
     def _group_by_table(self):
-        self.table_const = self.table_const.groupby(['schema_name', 'table_name'])['col_sql'].apply(
+        print(self.tables.info())
+        self.tables = self.tables.groupby(['schema_name', 'table_name'])['col_sql'].apply(
             ',\n'.join).reset_index()
 
     def _set_create_table(self):
         self.table_const['create_table'] = self.table_const.apply(
-            lambda x: f"CREATE TABLE {x['schema_name']}.{x['table_name']}(\n{x['col_sql']});", axis=1)
+            lambda x: f"CREATE TABLE {x['schema_name']}.{x['table_name']}\n(\n{x['col_sql']} {x['const_sql']}\n);", axis=1)
 
     def _set_paths(self):
-        self.table_const['paths'] = self.table_const['schema_name'].apply(lambda x: self.folders.schema_path / x / "tables")
+        self.table_const['paths'] = self.table_const['schema_name'].apply(
+            lambda x: self.folders.schema_path / x / "tables")
 
     def _make_dirs(self):
         self.table_const['paths'].apply(lambda x: x.mkdir(exist_ok=True, parents=True))
 
     def _make_files(self):
         self.table_const.apply(
-            lambda x: x['paths'].joinpath(f"{x['schema_name']}.{x['table_name']}.sql").write_text(x['create_table']), axis=1)
+            lambda x: x['paths'].joinpath(f"{x['schema_name']}.{x['table_name']}.sql").write_text(x['create_table']),
+            axis=1)
