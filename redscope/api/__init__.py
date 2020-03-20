11af29ed4b4acfc0443ec9653fdc240332b9ea54
jessemaitland <jesse.maitland@heyjobs.de>
2020-03-20 13:03:59 +0100

    "update to release version 0.2.10

--- redscope/api/__init__.py
+++ redscope/api/__init__.py
@@ -1,3 +1,6 @@
 # flake8: noqa
 from redscope.features.schema_introspection.db_introspection import introspect_constraints, introspect_users, \
     introspect_tables, introspect_views, introspect_db, introspect_groups, introspect_schemas, introspect_user_groups
+
+from redscope.env import load_redscope_env
+from redscope.database.db_connections import get_db_connection
