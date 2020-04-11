abacf829f195f9e3f40516e9a69fe34afec218cd
jessemaitland <jesse.maitland@heyjobs.de>
2020-04-11 21:43:32 +0200

    "update to release version 0.3.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-VERSION = '0.3.4'
+VERSION = '0.3.5'
 
 
 class VerifyVersionCommand(install):
